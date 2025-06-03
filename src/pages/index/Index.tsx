6b2012093ca5f4dd760cf7e6f7234e78870dabd3
Daniil Pugin <v0ltmx@yandex.ru>
2025-06-03 18:59:26 +0300

    Text on slider

--- src/pages/index/Index.tsx
+++ src/pages/index/Index.tsx
@@ -307,7 +307,10 @@ const Index = () => {
             />
             <div className="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center">
               <div className="text-center text-white">
-                
+                <h2 className="text-4xl font-bold mb-2">
+                  {banners[currentSlide].title}
+                </h2>
+                <p className="text-xl">{banners[currentSlide].subtitle}</p>
               </div>
             </div>
           </div>
